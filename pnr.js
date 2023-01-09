0a6f6642383435b59bd1f9b6ab19f2ff17ed001e
masif2002 <masif2002@outlook.com>
2023-01-09 16:45:51 +0530

    Added status checkig functionality

---
+++ pnr.js
@@ -0,0 +1,48 @@
+const getData = () => {
+    const pnr = document.getElementById('pnr-no').value
+    
+    const pnr_json = {
+        'pnr': pnr
+    }
+    
+    const value = JSON.stringify(pnr_json)
+    sendJsonData(value);
+}
+
+const sendJsonData = (data) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "YOUR_API_ENDPOINT"); 
+
+    xhr.onload = function(event){ 
+        const data = JSON.parse(xhr.response)
+
+        if (xhr.status != 200) {
+            displayErrorMessage(data.status)
+        }
+    }; 
+    
+    var res = localStorage.getItem('response')
+    var token = JSON.parse(res).token
+    
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.setRequestHeader('Authorization', token)
+    xhr.send(data);
+}
+
+const displayErrorMessage = (message) => {
+
+    if (document.getElementById('error-message')) { 
+        const m = document.getElementById('error-message')
+        m.parentNode.removeChild(m)
+    } 
+        const msg = document.createElement('p')
+        msg.id = "error-message"
+        msg.textContent = message
+        msg.classList.add('shake')
+
+
+    document.querySelector('.pnr-container').appendChild(msg)
+}
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', getData)
