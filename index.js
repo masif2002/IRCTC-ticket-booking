d1222251d76e550de12bc72c23b9a8b91e4eb8d9
shrikanth <shrikanthdeva02@gmail.com>
2023-02-13 09:28:53 +0530

    Changed login.js index.js login.html

--- index.js
+++ index.js
@@ -1,194 +1,216 @@
-const nextPage = () => {
-    addMoveOutTransitionEffect();
-
-    addPasswordField();
-    addMoveInTransitionEffect();
-
-    addButton('Back', 'M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z');
-    removeNextButton();
-    addSubmitButton();
+// min date fixer for calender
+var date_pick = document.getElementById("datePickerId")
+if (date_pick)
+{
+    datePickerId.min = new Date().toISOString().split("T")[0];
+    document.getElementById("datePickerId").valueAsDate = new Date()
 }
 
-const prevPage = () => {
-    console.log('Hi')
-
-    document.getElementById('username').classList.remove('move-out-left')
-    document.getElementById('username').classList.remove('move-in-right')
-    document.getElementById('username').classList.add('move-in-right')
-    document.getElementById('password').classList.remove('move-in-left')
-    document.getElementById('password').classList.add('move-out-right')
-
-    // Remove Back Button
-    const back = document.getElementById('Back')
-    document.getElementById('button-container').removeChild(back)
-
-    // Remove Submit Button
-    const submit = document.getElementById('submit')
-    document.getElementById('button-container').removeChild(submit)
-
-    // Add Next Button
-    addButton('Next', 'M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z')
-
-    document.getElementById('button-container').classList.remove('justify-space-between')
-    
-}
-
-const removeUsernameField = () => {
-    const username = document.getElementById('username');
-    document.getElementById('credentials-form').removeChild(username)
-}
-
-const addMoveOutTransitionEffect = () => {
-    // document.getElementById('username').classList.remove('move-out-left')
-    // document.getElementById('username').classList.remove('animation-delay')
-    // document.getElementById('username').classList.remove('move-reverse')
-    document.getElementById('username').classList.remove('move-in-right')
-    document.getElementById('username').classList.add('move-out-left')
-
-
-    // console.log(document.getElementById('username').classList)
-}
-
-const addMoveInTransitionEffect = () => {
-    document.getElementById('password').classList.remove('move-out-right')
-    document.getElementById('password').classList.remove('move-in-left')
-    document.getElementById('password').classList.add('move-in-left')
-}
-
-const addPasswordField = () => {
-
-    if (document.getElementById('password')) {
-        console.log("Element already exists")
-        return
-    }
-    const password = document.createElement('input')
-    password.type = 'password';
-    password.name = "password";
-    password.id = "password";
-    password.placeholder = "Enter password";
-    
-    document.getElementById('credentials-form').appendChild(password)
-    
-}
-
-const addButton = (buttonType, pathForSvg) => {
-
-    const primaryButtonClass = 'primary next-btn'
-    const secondaryButtonClass = 'secondary back-btn'
-
-    const button = document.createElement('button')
-    button.className = buttonType == 'Next' ? primaryButtonClass : secondaryButtonClass 
-    button.id = buttonType
-
-    const text = document.createElement('span')
-    text.innerHTML = buttonType
-
-    // adding BACK ICON 
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('fill', 'currentColor')
-    svg.setAttribute('height', '18')
-    svg.setAttribute('width', '18')
-    svg.setAttribute('viewBox', '0 0 16 16')
-
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-    path.setAttribute('d', pathForSvg)
-    svg.appendChild(path)
-
-    if (buttonType == 'Next') {
-        svg.classList.add('margin-left-2')
-        button.appendChild(text)
-        button.appendChild(svg)
-    } else {
-        button.appendChild(svg)
-        button.appendChild(text)
-    }
-
-    document.getElementById('button-container').appendChild(button)
-    if (buttonType == 'Back') {
-        document.getElementById('Back').addEventListener('click', prevPage)
-    } else {
-        document.getElementById('Next').addEventListener('click', nextPage)
-    }
-}
-
-const removeNextButton = () => {
-    const next = document.getElementById('Next')
-    document.getElementById('button-container').removeChild(next)
-}
-
-const addSubmitButton = () => {
-    const button = document.createElement('button')
-    button.className = 'primary next-btn'
-    button.id = "submit"
-    button.type = "submit"
-    button.value = "Submit"
-    // button.setAttribute('form', 'credentials-form')
-    button.innerHTML = 'Submit'
-
-    document.getElementById('button-container').appendChild(button)
-    document.getElementById('button-container').classList.add('justify-space-between')
-}
-
-const sendFormData = () => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://10.140.11.221:5000/user/login"); 
-
-    // This fires up when the connection is successful
-    xhr.onload = function(event){ 
-        // alert("Success, server responded with: " + event.target.response);
-        localStorage.clear() 
-        localStorage.setItem('response', xhr.response)
-        console.log(xhr.response)
-        // console.log(xhr.status)
-        displayStatus(xhr.status)
-    }; 
-
-    var formData = new FormData(document.getElementById("credentials-form")); 
-    xhr.send(formData);
-}
-
-
-
-
-
-const displayStatus = (statusCode) => {
-    const res = localStorage.getItem('response')
-    // console.log(response)
-    const status = JSON.parse(res).status
-
-    if (document.getElementById('status')) {
-        const displayMessage = document.getElementById('status')
-        displayMessage.parentNode.removeChild(displayMessage)
-    }
-    
-    if (statusCode == 200) {
-        window.location.href = "/index.html"
-    } else {
+// function display missing fields
+function displayStatus2()
+{
+    var from = document.getElementById("From").value;
+    var to = document.getElementById("To").value;
+    if (from == "" || to == "")
+    {
+        var status2 = document.getElementById('status2') 
+        if (status2) {
+            const displayMessage = document.getElementById('status2')
+            displayMessage.parentNode.removeChild(displayMessage)
+        }
+        
+        var status2 = "*Missing Fields"
         const newNode = document.createElement('p')
-        //     console.log(res)
-        newNode.id = 'status'
+        newNode.id = 'status2'
         newNode.classList.add('shake') 
-
-        const displayText = document.createTextNode(status)
+    
+        const displayText = document.createTextNode(status2)
         newNode.appendChild(displayText)
     
-        const buttons = document.getElementById('button-container')   
+        const buttons = document.getElementById('search')   
         const parent = buttons.parentNode
-    
         parent.insertBefore(newNode, buttons)
+        return false
+               
     }
-
+    var status2 = document.getElementById('status2') 
+    if (status2) {
+        const displayMessage = document.getElementById('status2')
+        displayMessage.parentNode.removeChild(displayMessage)
+    }
+    return true
 }
 
-document.getElementById('Next').addEventListener('click', nextPage)
+// form to json serialize
+function json_serialize()
+{
+
+    let val =  displayStatus2();
+    if (val)
+    {
+        let form_result = {}
+        form_result.trainfrom = document.getElementById("From").value;    
+        form_result.trainto = document.getElementById("To").value;    
+        form_result.type = document.getElementById("TicketType").value;    
+        form_result.class = document.getElementById("TicketClass").value;    
+        form_result.date = document.getElementById("datePickerId").value;  
+        if (document.getElementById("AvailableTrain").checked)  
+        {
+            form_result.trainavailable = document.getElementById("AvailableTrain").value;  
+        }
+        else
+        {
+            form_result.AvailableTrain = "no";
+        }
+        // console.log(form_result);
+        let jsonStringObj = JSON.stringify(form_result);
+        console.log(jsonStringObj);
+        console.log("from+index.js "+localStorage.getItem("token"))
+        let url = "http://192.168.7.203:5000/"
+
+        fetch(url+'user/home', {
+    
+        method: 'POST', // or 'PUT'
+        headers: {    
+            'Authorization':'Bearer '+localStorage.getItem("token"),
+                'Accept': 'application/json,text/plain,*/*',
+                'Content-Type': 'application/json'      
+            },
+            body: jsonStringObj
+    
+        }).then((response) => response.json())
+        .then((data) => {
+            
+                
+                localStorage.setItem("response",data)
+                console.log(data)
+                if(data)
+                {
+                    displayTrains(data)
+                }
+                else
+                {
+                    displayNoresult()
+                }
+                
+                // console.log(data[0]['time'])
+                // localStorage.setItem('time',data[0]['time'])
+                // window.location.href="/trains.html"
+                // var dur=document.createElement('p');
+                // dur.innerHTML='html'
 
 
-// This is because the normal event listener gets clicked automatically from the Next button because they are in the same position. Cannot use "document.getElementById('submit').addEventListener('click', sendFormData())"
-document.addEventListener( "click", someListener );
 
-function someListener(event){
-    var element = event.target;
-    if(element.id == 'submit' && element.type == "submit"){
-        sendFormData()
+            }).catch((error) => {
+                console.dir('Error:', error);
+        });
+    
+
+
+
     }
+}
+
+// function showdata()
+// {
+//     let data  = localStorage.getItem("response")
+//     const newNode = createElement("p")
+//     newNode.id = "data"
+//     const displayText = document.createTextNode(data)
+
+
+// }
+
+
+function switch_inputs()
+{
+    var from = document.getElementById("From").value;
+    var to = document.getElementById("To").value;
+
+    var temp = from;
+    from = to;
+    to = temp;
+
+    document.getElementById("From").value = from;
+    document.getElementById("To").value = to;
+       
+}
+    
+
+
+var switch_input = document.getElementById("switch_inputs")
+if (switch_input)
+{
+    document.getElementById("switch_inputs").addEventListener('click',switch_inputs)  
+}
+
+
+var search_btn = document.getElementById("search")
+if (search_btn)
+{
+    document.getElementById("search").addEventListener('click',json_serialize)    
+    
+}
+
+function displayTrains(data)
+{
+    data.forEach(ele => { 
+        let train = document.createElement("div")
+        train.classList.add("train")
+
+        // trainname trainid duration starttime startdate trainfrom trainto enddate endtime
+        
+        // top
+        let top = document.createElement("div")
+        top.classList.add("top")
+
+        let train_name = document.createElement("p")
+        train_name.innerHTML = ele["trainname"]
+        let train_id = document.createElement("p")
+        train_id.innerHTML = ele["trainid"]
+        top.appendChild(train_name)
+        top.appendChild(train_id)
+
+        // bottom
+
+        // start
+        let bottom = document.createElement("div")
+        top.classList.add("bottom")
+
+        let start_time = document.createElement("p")
+        train_from.innerHTML = ele["starttime"]
+        let train_from = document.createElement("p")
+        train_from.innerHTML = ele["trainfrom"]
+        let start_date = document.createElement("p")
+        train_from.innerHTML = ele["startdate"]
+
+        // duration
+        let duration = document.createElement("p")
+        duration.innerHTML = ele["duration"]
+
+        // to
+        let end_time = document.createElement("p")
+        train_from.innerHTML = ele["endtime"]
+        let train_to = document.createElement("p")
+        train_to.innerHTML = ele["trainto"]
+        let end_date = document.createElement("p")
+        train_from.innerHTML = ele["enddate"]
+
+
+        bottom.appendChild(start_time)
+        bottom.appendChild(train_from)
+        bottom.appendChild(start_date)
+        bottom.appendChild(duration)
+        bottom.appendChild(end_time)
+        bottom.appendChild(train_to)
+        bottom.appendChild(end_date)
+
+
+
+        train.appendChild(top)
+        train.appendChild(bottom)
+
+        document.querySelector("trains").appendChild(train)
+
+    });
 }
